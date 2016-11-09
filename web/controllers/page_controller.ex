7797eae7038cd0913643a6ee2519a28f0e565f0a
thaoho21294 <thaoho21294@gmail.com>
2016-11-09 23:42:56 +0700

    add create trip function

--- web/controllers/page_controller.ex
+++ web/controllers/page_controller.ex
@@ -4,4 +4,10 @@ defmodule BsnWeb.PageController do
   def index(conn, _params) do
     render conn, "index.html"
   end
+  def view_trip(conn, _params) do
+  	render conn, "view-trip.html"
+  end
+  def create_trip(conn, _params) do
+  	render conn, "create-trip.html"
+  end
 end
