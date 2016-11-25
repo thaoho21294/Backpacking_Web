6de42ed69328a6310a3a36622ddbd76789adff6c
thaoho21294 <thaoho21294@gmail.com>
2016-11-26 00:13:35 +0700

    add autocomplete function

--- web/views/map_view.ex
+++ web/views/map_view.ex
@@ -4,4 +4,8 @@ defmodule BsnWeb.MapView do
 	def render("get_autocomplete_data.json", %{address: address}) do
 		%{address: Map.get(address,"predictions")}
 	end
+	def render("get_location_data.json", %{location: location}) do
+		#.result.geometry.location
+		%{location: Map.get(Map.get(Map.get(location,"result"),"geometry"),"location")}
+	end
 end
