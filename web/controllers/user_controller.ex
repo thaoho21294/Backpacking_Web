3e1257f7e78b35e91bc2b002088e46f9e1988a2a
thaoho21294 <thaoho21294@gmail.com>
2017-01-03 16:41:35 +0700

    Unfinished login function

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -7,4 +7,14 @@ defmodule BsnWeb.UserController do
   	friends_name=Neo4j.query!(Neo4j.conn, cypher)
   	render(conn, "friends_name.json",friends_name: friends_name)
   end
+  def check_login(conn, %{"email"=>email, "password"=>password}) do
+  	cypher="MATCH (u:User) WHERE u.email=\"#{email}\" and u.pass=\"#{password}\" return id(u) as id"
+  	users=Neo4j.query!(Neo4j.conn, cypher)
+  	if Enum.empty?(users) do
+  		render(conn, "login.html", message: "Sai mật khẩu hoặc email!")
+  	else
+  		conn=put_session(conn, :userid, Map.get(Enum.at(users, 0), 'id'))
+  		redirect conn, to: "/"
+  	end
+  end
 end
